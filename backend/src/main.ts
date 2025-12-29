2b1fc3a2dcdc184f7be4109a01504216529247ee
Nitro Kevin <kv937398@gmail.com>
2025-12-28 21:16:15 -0300

    feat: global validation to the whole app

---
+++ backend/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true, // This is the key line
+    transformOptions: {
+      enableImplicitConversion: true, // Allows conversion based on TypeScript types
+    },
+  }));
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
