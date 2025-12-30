2726c2fd28b89e49b1f90d0e4f149fe58a7aa1df
Nitro Kevin <kv937398@gmail.com>
2025-12-30 16:04:23 -0300

    feat: delete create user controller cause there is already one in the auth controller

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,21 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Body, Patch, Param, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { GetUser } from '@/auth/decorators/get-user.decorator';
 import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
-import { GuestGuard } from '@/auth/guards/guest.guard';
-import { OptionalJwtGuard } from '@/auth/guards/optional-jwt.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
-
-  @UseGuards(OptionalJwtGuard, GuestGuard)
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
 
   @Get()
   findAll() {
