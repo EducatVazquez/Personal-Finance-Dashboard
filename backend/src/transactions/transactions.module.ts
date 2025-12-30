3434b537638eccc99a32adef342bd8a995fd334b
Nitro Kevin <kv937398@gmail.com>
2025-12-30 16:43:18 -0300

    feat: add user schema to be able to use it in the authentication resources

--- backend/src/transactions/transactions.module.ts
+++ backend/src/transactions/transactions.module.ts
@@ -4,11 +4,13 @@ import { TransactionsController } from './transactions.controller';
 import { AuthModule } from '@/auth/auth.module';
 import { TransactionSchema } from './schemas/transaction.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from '@/users/schemas/user.schema';
 
 @Module({
   imports: [
     AuthModule,
-    MongooseModule.forFeature([{ name: 'Transaction', schema: TransactionSchema }])
+    MongooseModule.forFeature([{ name: 'Transaction', schema: TransactionSchema }]),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
   ],
   controllers: [TransactionsController],
   providers: [TransactionsService],
