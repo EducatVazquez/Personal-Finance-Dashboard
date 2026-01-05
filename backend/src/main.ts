e33e4c1519be3d6adaacfdc399e418192ff5d083
Nitro Kevin <kv937398@gmail.com>
2026-01-05 16:32:12 -0300

    feat: add new config to the main app code

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,6 +9,8 @@ async function bootstrap() {
     transformOptions: {
       enableImplicitConversion: true, // Allows conversion based on TypeScript types
     },
+    forbidNonWhitelisted: true,
+    whitelist: true
   }));
   await app.listen(process.env.PORT ?? 3000);
 }
