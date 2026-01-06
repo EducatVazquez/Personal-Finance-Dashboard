1c134e6dec4ef4b00e79ae76bafdd06769aada4d
Nitro Kevin <kv937398@gmail.com>
2026-01-05 21:09:19 -0300

    feat: add ObjectId and entity document to make it be able to summon the entity itself just by the property

--- backend/src/categories/interfaces/ICategories.interface.ts
+++ backend/src/categories/interfaces/ICategories.interface.ts
@@ -1,6 +1,9 @@
+import { Types } from "mongoose";
+import { UserDocument } from "@/users/interfaces/users.interface";
+
 export interface ICategory {
     name: string;
-    userId?: string | null;
+    userId?: string | null | Types.ObjectId | UserDocument;
     description?: string;
     createdAt?: Date;
     updatedAt?: Date;
--- backend/src/transactions/interfaces/ITransactions.interface.ts
+++ backend/src/transactions/interfaces/ITransactions.interface.ts
@@ -1,6 +1,10 @@
+import { CategoryDocument } from "@/categories/interfaces/ICategoriesDocument.interface";
+import { UserDocument } from "@/users/interfaces/users.interface";
+import { Types } from "mongoose";
+
 export interface ITransaction {
-    userId: string;
-    category_id?: string;
+    userId: string | Types.ObjectId | UserDocument;
+    category_id?: string | Types.ObjectId | CategoryDocument;
     amount: number;
     date: Date;
     type: string;
