ab7fd0f5d3e568a236fbcd1b6083b138361828ca
Nitro Kevin <kv937398@gmail.com>
2026-01-05 21:40:46 -0300

    feat: implement the use of jwt guard to make the controller be able to access properties from token

--- backend/src/categories/categories.controller.ts
+++ backend/src/categories/categories.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
+import { GetUser } from '@/auth/decorators/get-user.decorator';
+import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) { }
 
   @Get()
-  findAll() {
-    return this.categoriesService.findAll();
+  @UseGuards(JwtAuthGuard)
+  findAll(@GetUser('userId') userId: string) {
+    return this.categoriesService.findAll(userId);
   }
 
 }
