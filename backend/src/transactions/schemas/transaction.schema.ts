9b9ff52451fd496b700bc8e2b085914616d0b7b6
Nitro Kevin <kv937398@gmail.com>
2026-01-05 21:14:34 -0300

    fix: change type of property for userId property to make it have ObjectId type and not only string

--- backend/src/transactions/schemas/transaction.schema.ts
+++ backend/src/transactions/schemas/transaction.schema.ts
@@ -3,8 +3,8 @@ import { ITransactionDocument } from '@/transactions/interfaces/ITransactionDocu
 import { TransactionType } from '../interfaces/ITransactions.interface';
 
 export const TransactionSchema = new mongoose.Schema<ITransactionDocument>({
-    userId: { type: String, required: true },
-    category_id: { type: String },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    category_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
     amount: { type: Number, required: true },
     type: { type: String, enum: Object.values(TransactionType), required: true, uppercase: true },
     date: { type: Date, required: true },
